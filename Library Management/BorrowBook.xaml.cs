16bf6b7ca7db08daf2d3da7e15656942250f0105
Huỳnh Đức Khoản <khoan0823@gmail.com>
2018-04-09 18:32:59 +0700

    Cập nhật giao diện ban đầu.
    Mất 2 tiếng 30'.

---
+++ Library Management/BorrowBook.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Library_Management
+{
+    /// <summary>
+    /// Interaction logic for BorrowBook.xaml
+    /// </summary>
+    public partial class BorrowBook : UserControl
+    {
+        List<User> items = new List<User>();
+
+        public BorrowBook()
+        {
+            InitializeComponent();
+            items.Add(new User() { Name = "123.com", Age = 42, Mail = "1@123.com" });
+            items.Add(new User() { Name = "123", Age = 39, Mail = "2@123.com" });
+            items.Add(new User() { Name = "Free Education", Age = 7, Mail = "3@123.com" });
+            items.Add(new User() { Name = "Free Educati23on", Age = 23, Mail = "3@123.com" });
+            lvUsers.ItemsSource = items;
+
+        }
+
+        public class User
+        {
+            public string Name { get; set; }
+
+            public int Age { get; set; }
+
+            public string Mail { get; set; }
+        }
+
+        private void MenuItem_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            int id = lvUsers.Items.IndexOf(lvUsers.SelectedItem);
+            MessageBox.Show(items[id].Name);
+        }
+    }
+}
---
+++ Library Management/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MahApps.Metro.Controls;
+
+namespace Library_Management
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// 
+
+
+
+    public partial class MainWindow : MetroWindow, INotifyPropertyChanged
+    {
+
+        #region  Test ToggleButton
+
+        private bool isBorrowBook, isBook, isStudent, isTransgress;
+
+        public bool IsBorrowBook { get => isBorrowBook; set { isBorrowBook = value; isBook = false; isStudent = false; isTransgress = false; OnPropertyChanged("IsBorrowBook"); OnPropertyChanged("IsBook"); OnPropertyChanged("IsStudent"); OnPropertyChanged("IsTransgress"); }  }
+        public bool IsBook { get => isBook; set { isBorrowBook = false; isBook = value; isStudent = false; isTransgress = false; OnPropertyChanged("IsBorrowBook"); OnPropertyChanged("IsBook"); OnPropertyChanged("IsStudent"); OnPropertyChanged("IsTransgress"); } }
+        public bool IsStudent { get => isStudent; set { isBorrowBook = false; isBook = false; isStudent = value; isTransgress = false; OnPropertyChanged("IsBorrowBook"); OnPropertyChanged("IsBook"); OnPropertyChanged("IsStudent"); OnPropertyChanged("IsTransgress"); } }
+        public bool IsTransgress { get => isTransgress; set { isBorrowBook = false; isBook = false; isStudent = false; isTransgress = value; OnPropertyChanged("IsBorrowBook"); OnPropertyChanged("IsBook"); OnPropertyChanged("IsStudent"); OnPropertyChanged("IsTransgress"); } }
+
+        private void github_Click(object sender, RoutedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://github.com/huynhduckhoan/LibraryManagement");
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string newName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(newName));
+            }
+        }
+        #endregion
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            // Test ToggleButton
+            this.DataContext = this;
+            isBorrowBook = true;
+        }
+
+    }
+}
